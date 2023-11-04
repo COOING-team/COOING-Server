e82d27492f8b63583d26e6ec61e1e3c03d2f1782
yujin <jini1514@soongsil.ac.kr>
2023-11-04 22:47:15 +0900

    setting

---
+++ src/main/java/com/example/cooing/CooingApplication.java
@@ -0,0 +1,13 @@
+package com.example.cooing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cooing/CooingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cooing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
