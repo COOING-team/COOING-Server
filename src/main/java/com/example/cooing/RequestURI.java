e67c014c9adb07ac02fd266838978c57a2bcc405
yujin <jini1514@soongsil.ac.kr>
2023-11-04 23:16:48 +0900

    setting

---
+++ src/main/java/com/example/cooing/RequestURI.java
@@ -0,0 +1,8 @@
+package com.example.cooing;
+
+public class RequestURI {
+
+    private static final String CURRENT_VER = "/api/v1";
+    public static final String AUTH_URI = CURRENT_VER + "/auth";
+
+}
