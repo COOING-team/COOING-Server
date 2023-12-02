027e206b16303cef87b5b6d156ebc9add007fc4f
yujin <jini1514@soongsil.ac.kr>
2023-12-03 00:04:15 +0900

    feat: 질문 CRUD 끝

--- src/main/java/com/example/cooing/domain/question/QuestionController.java
+++ src/main/java/com/example/cooing/domain/question/QuestionController.java
@@ -1,17 +1,19 @@
 package com.example.cooing.domain.question;
 
-import static com.example.cooing.global.RequestURI.ANSWER_URI;
 import static com.example.cooing.global.RequestURI.QUESTION_URI;
 
-import com.example.cooing.domain.auth.CustomUserDetails;
+import com.example.cooing.domain.question.dto.CreateQuestionRequest;
 import com.example.cooing.domain.question.dto.QuestionResponseDto;
-import com.example.cooing.global.base.BaseResponseDto;
+import com.example.cooing.global.entity.Question;
 import com.example.cooing.global.exception.CustomException;
+import com.example.cooing.global.base.BaseResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,17 +24,48 @@ public class QuestionController {
 
   private final QuestionService questionService;
 
-
-  @GetMapping(value = "/{questionId}")
-  @Operation(summary = "해당 질문 반환", description = "홈에 있는 cooingDay에 +1 한 값을 입력하면 그 다음 질문 번호가 되겠죠?")
-  public BaseResponseDto<QuestionResponseDto> home(@PathVariable("questionId") Long questionId) {
+  @GetMapping(value = "/all")
+  @Operation(summary = "전체 질문 반환", description = "모든 질문을 반환")
+  public BaseResponseDto<List<Question>> getAllQuestion() {
     try {
-      QuestionResponseDto questionResponseDto = questionService.getQuestion(questionId);
+      return BaseResponseDto.success("질문 조회 성공", questionService.getAllQuestion());
+    } catch (CustomException e) {
+      // 실패 시
+      return BaseResponseDto.fail(e.getCustomErrorCode().getCode(), e.getMessage());
+    }
+  }
+  @GetMapping(value = "/{cooingIndex}")
+  @Operation(summary = "해당 질문 반환", description = "홈에 있는 cooingDay에 +1 한 값을 입력하면 그 다음 질문 번호가 되겠죠?")
+  public BaseResponseDto<QuestionResponseDto> getQuestion(@PathVariable("cooingIndex") Long cooingIndex) {
+    try {
+      QuestionResponseDto questionResponseDto = questionService.getQuestion(cooingIndex);
       return BaseResponseDto.success("질문 조회 성공", questionResponseDto);
     } catch (CustomException e) {
       // 실패 시
       return BaseResponseDto.fail(e.getCustomErrorCode().getCode(), e.getMessage());
     }
   }
-
+  @PostMapping(value = "/create")
+  @Operation(summary = "해당 질문 추가", description = "질문을 만들어요")
+  public BaseResponseDto<QuestionResponseDto> saveQuestion(
+      CreateQuestionRequest createQuestionRequest) {
+    try {
+      QuestionResponseDto questionResponseDto = questionService.createQuestion(createQuestionRequest);
+      return BaseResponseDto.success("질문 저장 성공", questionResponseDto);
+    } catch (CustomException e) {
+      // 실패 시
+      return BaseResponseDto.fail(e.getCustomErrorCode().getCode(), e.getMessage());
+    }
+  }
+  @DeleteMapping(value = "/{cooingIndex}")
+  @Operation(summary = "해당 질문 삭제", description = "질문을 삭제해요")
+  public BaseResponseDto<Void> deleteQuestion(@PathVariable("cooingIndex") Long cooingIndex) {
+    try {
+      questionService.deleteQuestion(cooingIndex);
+      return BaseResponseDto.success("질문 삭제 성공", null);
+    } catch (CustomException e) {
+      // 실패 시
+      return BaseResponseDto.fail(e.getCustomErrorCode().getCode(), e.getMessage());
+    }
+  }
 }
--- src/main/java/com/example/cooing/domain/question/QuestionService.java
+++ src/main/java/com/example/cooing/domain/question/QuestionService.java
@@ -1,17 +1,16 @@
 package com.example.cooing.domain.question;
 
+import static com.example.cooing.global.exception.CustomErrorCode.EXIST_QUESTION;
+
+import com.example.cooing.domain.question.dto.CreateQuestionRequest;
 import com.example.cooing.domain.question.dto.QuestionResponseDto;
-import com.example.cooing.global.entity.Baby;
 import com.example.cooing.global.entity.Question;
-import com.example.cooing.global.entity.User;
 import com.example.cooing.global.exception.CustomErrorCode;
 import com.example.cooing.global.exception.CustomException;
-import com.example.cooing.global.repository.AnswerRepository;
-import com.example.cooing.global.repository.BabyRepository;
 import com.example.cooing.global.repository.QuestionRepository;
-import com.example.cooing.global.repository.UserRepository;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,6 +19,10 @@ public class QuestionService {
 
   private final QuestionRepository questionRepository;
 
+  public List<Question> getAllQuestion() {
+
+    return questionRepository.findAll();
+  }
   public QuestionResponseDto getQuestion(Long cooingIndex) {
 
     Question question = questionRepository.findByCooingIndex(cooingIndex)
@@ -30,5 +33,44 @@ public class QuestionService {
         .cooingIndex(question.getCooingIndex())
         .content(question.getContent())
         .build();
+  }
+
+  public QuestionResponseDto createQuestion(CreateQuestionRequest createQuestionRequest) {
+    // 이미 존재하는지 확인
+    Optional<Question> existingQuestion = questionRepository.findByCooingIndex(createQuestionRequest.getCooingIndex());
+
+    if (existingQuestion.isPresent()) {
+      // 이미 존재하면 예외 처리
+      throw new CustomException(EXIST_QUESTION);
+    } else {
+      // 없으면 새로 만들어 저장
+      Question question = saveQuestion(createQuestionRequest);
+
+      // 새로 만들어진 Question을 반환
+      return QuestionResponseDto.builder()
+          .questionId(question.getId())
+          .cooingIndex(question.getCooingIndex())
+          .content(question.getContent())
+          .build();
+    }
+  }
+
+  public Question saveQuestion(CreateQuestionRequest createQuestionRequest) {
+
+    Question q = Question.builder()
+        .content(createQuestionRequest.getContent())
+        .cooingIndex(createQuestionRequest.getCooingIndex())
+        .build();
+    questionRepository.saveAndFlush(q);
+    return q;
+  }
+
+  public void deleteQuestion(Long cooingIndex) {
+
+    //지우려고 하는데 없으면 에러
+    Question question = questionRepository.findByCooingIndex(cooingIndex)
+        .orElseThrow(() -> new CustomException(CustomErrorCode.NO_QUESTION));
+
+    questionRepository.delete(question);
   }
 }
