c797f5dc12e05163f325c0fcd3191b6cd671b4ef
yujin <jini1514@soongsil.ac.kr>
2023-12-03 03:44:49 +0900

    fix: 아이 두명 등록시 에러터질거 방지함
    인덱스0으로 박음

--- src/main/java/com/example/cooing/domain/auth/AuthService.java
+++ src/main/java/com/example/cooing/domain/auth/AuthService.java
@@ -30,86 +30,86 @@ import static com.example.cooing.global.exception.CustomErrorCode.NO_BABY;
 @Transactional
 public class AuthService {
 
-    private final JwtService jwtService;
+  private final JwtService jwtService;
 
-    private final UserRepository userRepository;
-    private final BabyRepository babyRepository;
-
-
-    @Transactional
-    public LoginResponseDto login(LoginRequest loginRequest) {
-
-        User user = userRepository.findByEmail(loginRequest.getEmail()).orElseGet(
-                () -> saveUser(loginRequest)
-        );
-        String serviceAccessToken = jwtService.createAccessToken(loginRequest.getEmail());
-        String serviceRefreshToken = jwtService.createRefreshToken(loginRequest.getEmail());
+  private final UserRepository userRepository;
+  private final BabyRepository babyRepository;
 
 
-        return LoginResponseDto.builder()
-                .accessToken(serviceAccessToken)
-                .refreshToken(serviceRefreshToken)
-                .build();
-    }
+  @Transactional
+  public LoginResponseDto login(LoginRequest loginRequest) {
 
-    @Transactional
-    public User saveUser(LoginRequest loginRequest) {
+    User user = userRepository.findByEmail(loginRequest.getEmail()).orElseGet(
+        () -> saveUser(loginRequest)
+    );
+    String serviceAccessToken = jwtService.createAccessToken(loginRequest.getEmail());
+    String serviceRefreshToken = jwtService.createRefreshToken(loginRequest.getEmail());
 
-        User member = User.builder()
-                .oAuthProvider(OAuthProvider.KAKAO)
-                .providerId(loginRequest.getProviderId())
-                .email(loginRequest.getEmail())
-                .role(Role.USER)
-                .name(loginRequest.getNickname())
-                .build();
-        userRepository.saveAndFlush(member);
-        return member;
+    return LoginResponseDto.builder()
+        .accessToken(serviceAccessToken)
+        .refreshToken(serviceRefreshToken)
+        .build();
+  }
 
-    }
+  @Transactional
+  public User saveUser(LoginRequest loginRequest) {
 
-    @Transactional
-    public InfoResponseDto getMyInfo(CustomUserDetails userDetails) {
-        User user = userRepository.findByEmail(userDetails.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다"));
+    User member = User.builder()
+        .oAuthProvider(OAuthProvider.KAKAO)
+        .providerId(loginRequest.getProviderId())
+        .email(loginRequest.getEmail())
+        .role(Role.USER)
+        .name(loginRequest.getNickname())
+        .build();
+    userRepository.saveAndFlush(member);
+    return member;
 
-        Baby baby = babyRepository.findByUserId(user.getId())
-                .orElseThrow(() -> new CustomException(NO_BABY));
+  }
 
-        return InfoResponseDto.builder()
-                .name(baby.getName())
-                .age(getAge(baby.getBirth()))
-                .month(getMonthsSinceBirth(baby.getBirth()))
-                .birth(baby.getBirth())
-                .sex(baby.getSex())
-                .build();
-    }
+  @Transactional
+  public InfoResponseDto getMyInfo(CustomUserDetails userDetails) {
+    User user = userRepository.findByEmail(userDetails.getEmail())
+        .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다"));
 
+    Baby baby = user.getBabyList().get(0); //Todo 아이 한명 이상이면 이 부분을 수정하세요
+//    Baby baby = babyRepository.findByUserId(user.getId())
+//        .orElseThrow(() -> new CustomException(NO_BABY));
 
-    @Transactional
-    public BabyResponseDto createBaby(CustomUserDetails userDetails, BabyRequest babyRequest) {
-        User user = userRepository.findByEmail(userDetails.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다"));
-
-        Baby baby = saveBaby(babyRequest);
-        user.addBaby(baby);
-        return BabyResponseDto.builder()
-                .name(babyRequest.getName())
-                .sex(babyRequest.getSex())
-                .birth(babyRequest.getBirth())
-                .build();
-    }
+    return InfoResponseDto.builder()
+        .name(baby.getName())
+        .age(getAge(baby.getBirth()))
+        .month(getMonthsSinceBirth(baby.getBirth()))
+        .birth(baby.getBirth())
+        .sex(baby.getSex())
+        .build();
+  }
 
 
-    @Transactional
-    public Baby saveBaby(BabyRequest babyRequest) {
+  @Transactional
+  public BabyResponseDto createBaby(CustomUserDetails userDetails, BabyRequest babyRequest) {
+    User user = userRepository.findByEmail(userDetails.getEmail())
+        .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다"));
 
-        Baby baby = Baby.builder()
-                .sex(babyRequest.getSex())
-                .birth(babyRequest.getBirth())
-                .createAt(LocalDateTime.now())
-                .name(babyRequest.getName())
-                .build();
-        babyRepository.saveAndFlush(baby);
-        return baby;
-    }
+    Baby baby = saveBaby(babyRequest);
+    user.addBaby(baby);
+    return BabyResponseDto.builder()
+        .name(babyRequest.getName())
+        .sex(babyRequest.getSex())
+        .birth(babyRequest.getBirth())
+        .build();
+  }
+
+
+  @Transactional
+  public Baby saveBaby(BabyRequest babyRequest) {
+
+    Baby baby = Baby.builder()
+        .sex(babyRequest.getSex())
+        .birth(babyRequest.getBirth())
+        .createAt(LocalDateTime.now())
+        .name(babyRequest.getName())
+        .build();
+    babyRepository.saveAndFlush(baby);
+    return baby;
+  }
 }
