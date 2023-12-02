9a465d87ffb9257dff1ac846331e0c4c9410554c
yujin <jini1514@soongsil.ac.kr>
2023-12-02 18:17:36 +0900

    feat: 익셉션 추가 중이었다

---
+++ src/main/java/com/example/cooing/global/CustomErrorResponse.java
@@ -0,0 +1,14 @@
+package com.example.cooing.global;
+
+import lombok.*;
+//
+//@Getter
+//@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Builder
+
+//public class CustomErrorResponse{
+//    private CustomErrorCode status;
+//    private String statusMessage;
+//}
