06cd244c49894e8a35cd0d0bf938a0d93c329c9f
yujin <jini1514@soongsil.ac.kr>
2023-12-03 18:36:40 +0900

    fix: 홈에 오늘 기록했는지 불리언값 이상했던거 오류 수정

--- src/main/java/com/example/cooing/domain/home/HomeService.java
+++ src/main/java/com/example/cooing/domain/home/HomeService.java
@@ -15,7 +15,9 @@ import com.example.cooing.global.exception.CustomException;
 import com.example.cooing.global.repository.AnswerRepository;
 import com.example.cooing.global.repository.BabyRepository;
 import com.example.cooing.global.repository.UserRepository;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -39,14 +41,16 @@ public class HomeService {
     Baby baby = babyRepository.findByUserId(user.getId())
         .orElseThrow(() -> new CustomException(NO_BABY));
 
-    Optional<Answer> answer = answerRepository.findByCreateAt(LocalDateTime.now());
+    Optional<Answer> answer = answerRepository.findByCreateAtBetweenAndBabyId(
+        LocalDate.now().atTime(LocalTime.MIN), LocalDate.now().atTime(LocalTime.MAX),
+        baby.getId()); //오늘 기록을 했나 안했나
     boolean isTodayRecord = answer.isPresent();
 
     return HomeResponseDto.builder()
         .name(baby.getName())
-        .cooingDay(answerRepository.countByBabyId(baby.getId()))
+        .cooingDay(answerRepository.countByBabyId(baby.getId())) //답변을 몇번이나 했는지 셈 -> 말을 기록한지 n일 째에요
         .month(getMonthsSinceBirth(baby.getBirth()))
-        .isTodayRecord(isTodayRecord)
+        .isTodayRecord(isTodayRecord) //
         .build();
   }
 
