827fa5908f03ee4f525f7e1ddfea34062f77e5cc
yujin <jini1514@soongsil.ac.kr>
2023-12-03 18:40:11 +0900

    fix: enddate max로 수정

--- src/main/java/com/example/cooing/global/util/CalculateYearAndMonthUtil.java
+++ src/main/java/com/example/cooing/global/util/CalculateYearAndMonthUtil.java
@@ -12,6 +12,6 @@ public class CalculateYearAndMonthUtil {
   //년과 월을 입력해서 그 월의 마지막 날짜를 반환합니다.
   public static LocalDateTime getMonthEndDate(int year, int month) {
     int lastDayOfMonth = LocalDate.of(year, month, 1).lengthOfMonth();
-    return LocalDateTime.of(year, month, lastDayOfMonth,0,0,0);
+    return LocalDateTime.of(year, month, lastDayOfMonth,23,59,59);
   }
 }
