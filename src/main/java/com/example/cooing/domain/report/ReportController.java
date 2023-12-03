e0290ca6bc9286f0e2784ccfa850a782dfb7ec17
k0000k <locker50@naver.com>
2023-12-03 16:07:51 +0900

    feat: secret note complete

--- src/main/java/com/example/cooing/domain/report/ReportController.java
+++ src/main/java/com/example/cooing/domain/report/ReportController.java
@@ -1,6 +1,7 @@
 package com.example.cooing.domain.report;
 
 import com.example.cooing.domain.auth.CustomUserDetails;
+import com.example.cooing.domain.report.dto.SecretNoteResponse;
 import com.example.cooing.global.base.BaseResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -19,7 +20,7 @@ public class ReportController {
 
     @GetMapping(value = "/secret-note")
     @Operation(summary = "해당 주차의 레포트 데이터를 받아옵니다.", description = "빈출 단어도 레포트를 생성해야만 갱신됩니다.")
-    public BaseResponseDto<ArrayList<Boolean>> SecretNote(@AuthenticationPrincipal CustomUserDetails userDetail,
+    public BaseResponseDto<SecretNoteResponse> SecretNote(@AuthenticationPrincipal CustomUserDetails userDetail,
                                                           @RequestParam Integer month,
                                                           @RequestParam Integer week) {
         return BaseResponseDto.success("ok", reportService.getSecretNote(userDetail, month, week));
